777d07014eb4121d87a6653bbdf673fca6fd39d6
Sili Wen <siliwen@amazon.com>
2016-08-07 19:19:42 -0700

    Use SFM instead of constructor if naming SFM is more clear and if the class requires multiple constructors with same signature

---
+++ src/chap2/creatinganddestroyingobjects/StaticFactoryMethodOverConstructors.java
@@ -0,0 +1,31 @@
+package chap2.creatinganddestroyingobjects;
+
+import java.math.BigInteger;
+
+public class StaticFactoryMethodOverConstructors {
+
+    void booleanValueOf() {
+        boolean b = Boolean.valueOf(false);
+    }
+
+    void confusingConstructor() {
+        BigInteger bigInt = new BigInteger(0, 0, null);
+        BigInteger preferableInstantiation = BigInteger.probablePrime(12, null);
+    }
+
+    class TestClass {
+
+        int val1;
+        int val2;
+
+        public TestClass(int a, Integer b) {
+            val1 = a;
+            val2 = b;
+        }
+
+        public TestClass(Integer b, int a) {
+            val1 = b;
+            val2 = a;
+        }
+    }
+}
