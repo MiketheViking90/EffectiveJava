3ecf14faf00e806979ee6701912ca6eccfe76c2b
Sili Wen <siliwen@gmail.com>
2016-08-10 19:56:02 -0700

    Add description to Enum Singleton

--- src/chap2/i3encoforcesingletonwithprivateconstructor/SingletonThree.java
+++ src/chap2/i3encoforcesingletonwithprivateconstructor/SingletonThree.java
@@ -7,9 +7,11 @@ public enum SingletonThree {
     INSTANCE;
 
     private String name = "The King";
+    private String description = "This is the preferred Singleton implementation";
 
     public static void main(String[] args) {
         SingletonThree singleton = SingletonThree.INSTANCE;
         System.out.println(singleton);
     }
+
 }
