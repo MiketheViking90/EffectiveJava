782eaa2a2525ae981161ba7c6897d6daecf8515b
Sili Wen <siliwen@gmail.com>
2016-09-13 20:36:12 -0700

    Use generic type over raw for compile-time type checking

---
+++ src/chap5/item23/StampCollector.java
@@ -0,0 +1,30 @@
+package chap5.item23;
+
+import chap2.item6.Collective;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class StampCollector {
+
+    private final Collection<Stamp> collection = new ArrayList<>();
+
+    public void collectStamps() {
+        collection.add(new Stamp());
+        collection.add(new Stamp());
+        collection.add(new Stamp());
+        collection.add(new Stamp());
+    }
+
+    public void iterateStamps() {
+        for (Stamp s : collection) {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        StampCollector collector = new StampCollector();
+        collector.collectStamps();
+        collector.iterateStamps();
+    }
+}
