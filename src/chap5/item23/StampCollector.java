8a29e5af5ffa8f100eb8d2471761f8bb6f33b2ab
Sili Wen <siliwen@gmail.com>
2016-09-14 19:56:55 -0700

    Use unbounded wildcard type in argument if do not know or care about the type, but places sever restrictions on the unbounded object

--- src/chap5/item23/StampCollector.java
+++ src/chap5/item23/StampCollector.java
@@ -1,7 +1,10 @@
 package chap5.item23;
 
+import com.google.common.collect.Sets;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 public class StampCollector {
 
@@ -20,9 +23,23 @@ public class StampCollector {
         }
     }
 
+    public static int elementsInCommon(Set<?> s1, Set<?> s2) {
+        int count = 0;
+        for (Object o : s1) {
+            if (s2.contains(o)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         StampCollector collector = new StampCollector();
         collector.collectStamps();
         collector.iterateStamps();
+
+        Set<String> s1 = Sets.newHashSet("a", "b", "c");
+        Set<String> s2 = Sets.newHashSet("a", "b");
+        elementsInCommon(s1, s2);
     }
 }
