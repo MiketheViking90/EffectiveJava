72f4d57b76b6956f15bf17b39b117d734340a7c6
Sili Wen <siliwen@gmail.com>
2016-10-04 19:49:27 -0700

    Use BigDecimal as alternative to float for 746 computations

--- src/chap8/item48/MoneyCalculation.java
+++ src/chap8/item48/MoneyCalculation.java
@@ -1,5 +1,7 @@
 package chap8.item48;
 
+import java.math.BigDecimal;
+
 public class MoneyCalculation {
 
     private static void floatingPointArithmetic() {
@@ -15,7 +17,24 @@ public class MoneyCalculation {
         System.out.println(funds + "$ in changes");
     }
 
+    private static void bigDecimalExactArithmetic() {
+        BigDecimal funds = new BigDecimal(1.00);
+        int itemsBought = 0;
+
+        BigDecimal tenCents = BigDecimal.valueOf(.10);
+        for (BigDecimal price = tenCents;
+             funds.compareTo(price) >= 0;
+             price = price.add(tenCents)) {
+            funds = funds.subtract(price);
+            itemsBought++;
+        }
+
+        System.out.println("bought: " + itemsBought + " items");
+        System.out.println(funds + "$ in change");
+    }
+
     public static void main(String[] args) {
-        floatingPointArithmetic();
+        //floatingPointArithmetic();
+        bigDecimalExactArithmetic();
     }
 }
